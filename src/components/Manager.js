36c210ba2a1a1189bcde39b324a71597d8a01550
Phamty2002 <114122026+Phamty2002@users.noreply.github.com>
2023-11-02 10:57:27 +0700

    Update Manager.js

--- src/components/Manager.js
+++ src/components/Manager.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useEffect,useState } from 'react';
 import './styles1.css';
+
+const API_URL = "https://942c-203-205-32-65.ngrok-free.app"
+
 const ManagerRequestList = () => {
+    const [requests, setRequests] = useState([]);
+
+    useEffect(() => {
+        fetch(API_URL + "/api/manager/requests", {
+            headers: {
+                'Authorization':"Bearer 2XbMGxdRyrdIXkkrCGccUwXnH4n_4h9phWZGR5BUZCW2XnKA4",
+                'ngrok-skip-browser-warning': 'any'
+            }
+        })
+        .then((response) => response.text())
+        .then((data) => {
+            console.log(data);
+            setRequests(data);
+        });
+    })
+
     // Sample list of requests (you can fetch this data from an API or state)
-    const requests = [
-        {
-            id: '',
-            name: '',
-            reason: '',
-            time: '',
-            fee: '',
-        },
-        // Add more requests as needed
-    ];
 
     const handleConfirm = (requestId) => {
         // Handle confirmation logic for the request with the given ID
